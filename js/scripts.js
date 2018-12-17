7173d7e90049a28b6958a3870aec1d8e78954334
OthiDaniel <othidaniel@gmail.com>
2018-12-17 06:32:14 +0300

    code for front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -72,3 +72,23 @@ $("button#new-game").click(function(event){
 
     $(".start-menu").show();
   });
+  $("button#player2-roll").click(function(event){
+    player2.roll = throwdice();
+    $("#die-roll-2").text(player2.roll);
+    player2.rollone();
+    $("#round-total-2").text(player2.tempscore);
+  });
+  $("button#player1-hold").click(function(event){
+    player1.hold();
+    $("#total-score-1").text(player1.totalscore);
+    $("#round-total-1").empty();
+    $("#die-roll-1").empty();
+    player1.winnerCheck();
+  });
+  $("button#player2-hold").click(function(event){
+    player2.hold();
+    $("#total-score-2").text(player2.totalscore);
+    $("#round-total-2").empty();
+    $("#die-roll-2").empty();
+    player2.winnerCheck();
+  });
