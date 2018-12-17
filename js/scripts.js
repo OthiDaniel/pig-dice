386fd7fb2b475ce79cdb64a7de0782c09ef06871
OthiDaniel <othidaniel@gmail.com>
2018-12-17 06:02:35 +0300

    add js code for backend

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var player1 = "";
+var player2 = "";
+
+var rollDice = function(){
+  return Math.floor(Math.random()*6)+1;
+}
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
+Player.prototype.rollOne = function () {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert ("Sorry " + this.playerName + ", you rolled 1! Player 2's turn!")
+  } else {
+    this.tempscore += this.roll;
+  }
+}
+Player.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  alert(this.playerName + ", your turn is over!")
+}
